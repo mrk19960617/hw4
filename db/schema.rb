56eb55043a910be0552938f1207f93eb980a68e2
mrk19960617 Lienhard <mark.lienhard@kellogg.northwestern.edu>
2025-03-14 01:10:03 +0000

    More changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_14_005852) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_14_010819) do
   create_table "entries", force: :cascade do |t|
     t.string "title"
     t.text "description"
